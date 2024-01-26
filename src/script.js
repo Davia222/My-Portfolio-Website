9950d1da3d124021de55f2c99912ffa50364950d
Davia222 <igwezedavid2@gmail.com>
2024-01-26 19:06:54 +0100

    First commit

---
+++ src/script.js
@@ -0,0 +1,17 @@
+let mouseInner = document.querySelector(".mouseInner")
+let mouseOuter = document.querySelector(".mouseOuter")
+
+window.addEventListener("mousemove",(e)=>{
+    let posX = e.clientX
+    let posY = e.clientY
+
+    mouseInner.style.left = posX +"px" 
+    mouseInner.style.top = posY + "px"
+
+    mouseOuter.style.left = posX + "px"
+    mouseOuter.style.top = posY + "px"
+})
+
+window.addEventListener("click",()=>{
+    mouseOuter.style.color = "white"
+})
